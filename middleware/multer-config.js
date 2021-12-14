b4c4c4393e7971240fd33a5d0c7a3b73c79e67a3
JulienPetit <julienpetit1@gmail.com>
2021-12-14 12:54:03 +0100

    multer

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const path = require('path')
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -17,4 +18,4 @@ const storage = multer.diskStorage({
   }
 });
 
-module.exports = multer({storage: storage}).single('image');
+module.exports = multer({storage: storage}).single('images');
