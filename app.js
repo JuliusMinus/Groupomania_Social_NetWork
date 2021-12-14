ed0c948d2ea6c68e8ab80ac704879c8f529aa501
JulienPetit <julienpetit1@gmail.com>
2021-12-14 19:34:52 +0100

    modifications path

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use("/api/auth", userRoutes);
+app.use("/api/users/", userRoutes);
 app.use('/api/papers', paperRoutes);
 app.use('/api/comments', commentRoutes);
 
